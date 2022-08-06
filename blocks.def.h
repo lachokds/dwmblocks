3747e5821e50706abdd8501f3c951248af8cd689
Jonathan Villatoro <lacho8713@gmail.com>
2022-08-05 23:41:49 -0600

    configured custom blocks

--- blocks.def.h
+++ blocks.def.h
@@ -3,9 +3,17 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"🔈", "~/.local/bin/volume",						0,		10},
+
+	/* Get weather forecast for current location */
+	{"", "~/.local/bin/get_forecast",					3600,		0},
+
+	{"🌡", "sensors | awk '/^temp1:/{print $2}'",				5,		0},
+
+	/*{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},*/
+	{"", "date +'%F (%a) %R'",						5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "|";
 static unsigned int delimLen = 5;
